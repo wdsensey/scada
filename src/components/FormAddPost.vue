908409ff0d3dabc4a0d07c6e261daf8aa983024c
Alexander Kuzmin <kuzmin1913@gmail.com>
2020-04-16 23:56:28 +0300

    WIP validations, optimezations

--- src/components/FormAddPost.vue
+++ src/components/FormAddPost.vue
@@ -3,13 +3,24 @@
     <div class="form-header">
       <h3 class="h3">
         <router-link tag="div" :to="{ name: 'Home' }">
-          <ArrowLeft/>
+          <svg class="arrow-left" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+            <!-- eslint-disable max-len -->
+            <path fill="#333" d="M8.503 6.891l.284.284c.189.189.189.494 0 .682L5.415 11.21h15.103c.266 0 .482.216.482.482v.402c0 .266-.216.482-.482.482H5.415l3.372 3.353c.189.188.189.493 0 .681l-.284.284c-.188.189-.493.189-.682 0l-4.68-4.66c-.188-.189-.188-.494 0-.682l4.68-4.66c.189-.19.494-.19.682 0z"/>
+          </svg>
         </router-link>
         new post
       </h3>
     </div>
-    <input class="input" type="text" placeholder="title" v-model="title">
-    <textarea class="input text" placeholder="text" v-model="body"></textarea>
+    <div class="input-wrapper">
+      <input class="input" type="text" placeholder="title" v-model="title">
+      <div class="error" v-if="$v.title.$dirty && !$v.title.required">Field is empty</div>
+      <div class="error" v-if="$v.title.$dirty && !$v.title.minLength">Title must have at least {{$v.title.$params.minLength.min}} letters.</div>
+    </div>
+    <div class="input-wrapper">
+      <textarea class="input text" placeholder="text" v-model="body"></textarea>
+      <div class="error" v-if="$v.body.$dirty && !$v.body.required">Field is empty</div>
+      <div class="error" v-if="$v.body.$dirty && !$v.body.minLength">Body must have at least {{$v.body.$params.minLength.min}} letters.</div>
+    </div>
     <div class="form-footer">
       <button class="btn-dark" type="submit">add a post</button>
       <button class="btn-transparent" type="submit" @click="cancel">cancel</button>
@@ -20,13 +31,10 @@
 
 <script>
 import axios from 'axios';
-import ArrowLeft from '@/components/ArrowLeft.vue';
+import { required, minLength } from 'vuelidate/lib/validators';
 
 export default {
   name: 'FormAddPost',
-  components: {
-    ArrowLeft,
-  },
   data() {
     return {
       title: '',
@@ -34,11 +42,25 @@ export default {
       formSubmitted: false,
     };
   },
+  validations: {
+    title: {
+      required,
+      minLength: minLength(4),
+    },
+    body: {
+      required,
+      minLength: minLength(10),
+    },
+  },
   methods: {
     submit() {
+      if (this.$v.$invalid) {
+        this.$v.$touch();
+        return;
+      }
       const data = {
         title: this.title,
-        body: this.text,
+        body: this.body,
       };
       axios.post('https://jsonplaceholder.typicode.com/posts', data)
         .then(() => {
--- src/components/PostsList.vue
+++ src/components/PostsList.vue
@@ -14,7 +14,10 @@
              :key="post.id"
              @click="$emit('sendModalData', post)">
           {{ post.title }}
-          <ArrowRight />
+          <svg class="arrow-right" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
+            <!-- eslint-disable max-len -->
+            <path fill="#333" d="M20.662 9.188l-.378.38c-.252.25-.252.657 0 .908l4.496 4.47H4.643c-.355 0-.643.288-.643.643v.536c0 .355.288.643.643.643H24.78l-4.496 4.47c-.252.251-.252.658 0 .91l.378.378c.251.251.658.251.91 0l6.24-6.214c.25-.251.25-.658 0-.91l-6.24-6.214c-.252-.25-.659-.25-.91 0z"/>
+          </svg>
         </div>
       </div>
       <div class="tiles" v-if="layout === 'tiles'">
@@ -25,7 +28,10 @@
           <div class="tile-wrapper">
             <img src="@/assets/img/post-img.png" alt="">
             <div class="title">{{ post.title }}</div>
-            <ArrowRight />
+            <svg class="arrow-right" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32">
+              <!-- eslint-disable max-len -->
+              <path fill="#333" d="M20.662 9.188l-.378.38c-.252.25-.252.657 0 .908l4.496 4.47H4.643c-.355 0-.643.288-.643.643v.536c0 .355.288.643.643.643H24.78l-4.496 4.47c-.252.251-.252.658 0 .91l.378.378c.251.251.658.251.91 0l6.24-6.214c.25-.251.25-.658 0-.91l-6.24-6.214c-.252-.25-.659-.25-.91 0z"/>
+            </svg>
           </div>
         </div>
       </div>
@@ -35,14 +41,9 @@
 </template>
 
 <script>
-import ArrowRight from '@/components/ArrowRight.vue';
-
 export default {
   props: ['posts'],
   name: 'PostsList',
-  components: {
-    ArrowRight,
-  },
   data() {
     return {
       layout: 'list',
