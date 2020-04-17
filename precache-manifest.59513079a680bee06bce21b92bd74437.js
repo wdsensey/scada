0c0ee973571b43c0b5d14235e4eeff890715ff90
Alexandr <kuzmin1913@gmail.com>
2020-04-17 13:22:55 +0300

    deploy

---
+++ precache-manifest.59513079a680bee06bce21b92bd74437.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9e399074ad57739fa12",
+    "url": "/scada/css/app.a8a5b5f6.css"
+  },
+  {
+    "revision": "3d743115646a0a903821",
+    "url": "/scada/css/chunk-vendors.d93480aa.css"
+  },
+  {
+    "revision": "707d80514918b540af02b7b7c01e4c68",
+    "url": "/scada/img/404.707d8051.svg"
+  },
+  {
+    "revision": "eda9fe30f243e52870e1585e04d5bd1d",
+    "url": "/scada/img/logo.eda9fe30.svg"
+  },
+  {
+    "revision": "4de8fc97b6f3b095f97dc3221c65d1c8",
+    "url": "/scada/img/menu-close.4de8fc97.svg"
+  },
+  {
+    "revision": "1f4ece073fbb8b9afeab296a75bb4df2",
+    "url": "/scada/img/menu-open.1f4ece07.svg"
+  },
+  {
+    "revision": "ed26825742c336cf1042b4b12b2bec70",
+    "url": "/scada/index.html"
+  },
+  {
+    "revision": "b9e399074ad57739fa12",
+    "url": "/scada/js/app.87ae1e8e.js"
+  },
+  {
+    "revision": "3d743115646a0a903821",
+    "url": "/scada/js/chunk-vendors.c82ffa2c.js"
+  },
+  {
+    "revision": "fd12c6d0b81cdb80d5218dcc3b65a633",
+    "url": "/scada/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/scada/robots.txt"
+  }
+]);
